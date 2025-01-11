f102b99368a779f771690c358da70e1810e297c8
annyconfessor <anny.confessor@gmail.com>
2025-01-11 18:36:48 -0300

    feat(routes): add new route with auth-based redirection to home or login

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,16 @@
 import styled from "styled-components"
 
 const StyledDiv = styled.div`
+width: 100px;
+height: 100px;
 color: #FFFF;
 `
 
 const Home = () => {
   return(
-    <StyledDiv>Home</StyledDiv>
+    <StyledDiv>
+      <h1>Home</h1>
+    </StyledDiv>
   )
 }
 
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,18 +1,27 @@
-import Authorization from '@/auth';
-import { currentToken } from '@/constants/config';
-import { Routes as Router, Route, Navigate } from 'react-router-dom';
+import LoginAuthorization from '@/auth';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from '@/pages/home';
+import { useEffect } from 'react';
+import { currentToken } from '@/constants/config';
 
 const AppRouter = () => {
+  const navigate = useNavigate()
+  const isToken = Boolean(currentToken.access_token)
+  
+  useEffect(() => {
+    if (isToken) {
+      navigate('/home');
+    }
+    if (!isToken) {
+      navigate('/login');
+    }
+  }, [isToken, navigate])
 
-  if(currentToken.access_token) {
-    return <Navigate to="/home" />
-  }
   return (
-    <Router>
-      <Route path="/" element={<Authorization/>} />
-      <Route path={"./home"} element={<Home />} />
-    </Router>
+      <Routes>
+        <Route path="/login" element={<LoginAuthorization/>} />
+        <Route path="/home" element={<Home/>} />
+      </Routes>
   );
 };
 
