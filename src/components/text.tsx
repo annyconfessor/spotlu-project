d229d584a8814ce441d8889936c30630b98c1a27
annyconfessor <anny.confessor@gmail.com>
2025-01-12 12:03:50 -0300

    fix: add type definition for variant prop in Text component

--- src/components/text.tsx
+++ src/components/text.tsx
@@ -1,53 +1,59 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
 import { variant } from 'styled-system'
 
+interface TextProps {
+  variant?: 'title' | 'heading' | 'subheading' | 'subtitle' | 'paragraph' | 'subParagraph';
+}
+
 // TODO: configurar theme
-const Text = styled.span`
-  ${() => css`
-      ${variant({
-        variants: {
-          title: {
-            fontSize: 28,
-            fontWeight: 600,
-            color: '##FFFFFF',
-            // Grey/tom-color-grey-000
-            // color: theme.colors.support.white,
-          },
-          heading: {
-            fontSize: 24, 
+const Text = styled.span<TextProps>`
+    ${variant({
+      variants: {
+        title: {
+          fontSize: 28,
+          fontWeight: 600,
+          color: '##FFFFFF',
+        },
+        heading: {
+          fontSize: 24, 
+          fontWeight: 700,
+          color: '#FFFFFF',
+        },
+        subheading: {
+          fontSize: 19 ,
+          fontWeight: 700, 
+          color: '#FFFFFF',
+          disable: {
+            color: "#949EA2"
+          }
+        },
+        subtitle: {
+          fontSize: 16,  
+          fontWeight: 400,
+          color: '#D3DADD',
+          bold: {
             fontWeight: 700,
             color: '#FFFFFF',
           },
-          subheading: {
-            fontSize: 19 ,
-            fontWeight: 700, 
-            color: '#FFFFFF',
-            disable: {
-              color: "#949EA2"
-              // Grey/tom-color-grey-500
-            }
-          },
-          subtitle: {
-            fontSize: 16,  
-            fontWeight: 400,
-            color: '#D3DADD',
-            // Grey/tom-color-grey-300
-            bold: {
-              fontWeight: 700,
-              color: '#FFFFFF',
-            },
-          },
-          paragraph: {
-            fontSize: 14,
-            fontWeight: 400,
-            color: '#FFFFFF',
-            bold: {
-              fontWeight: 500
-            }
+        },
+        paragraph: {
+          fontSize: 14,
+          fontWeight: 400,
+          color: '#FFFFFF',
+          bold: {
+            fontWeight: 500
+          }
+        },
+        subParagraph: {
+          fontSize: 12,
+          fontWeight: 400,
+          color: '#FFFFFF',
+          bold: {
+            fontWeight: 500
           }
         }
-      })}
-  `}
+      }
+    })}
 `
 
 export default Text
