3ca319b2696236d00667bca9f4ed95c2bd33ee3f
annyconfessor <anny.confessor@gmail.com>
2025-01-24 16:54:11 -0300

    fix: jest configuration

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,20 +1,25 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  preset: "ts-jest", // Usa o ts-jest para suporte ao TypeScript
+  extensionsToTreatAsEsm: [".ts", ".tsx"], // Trata arquivos TypeScript como módulos ES
+  testEnvironment: 'jsdom', // Simula o ambiente do navegador para testes
   transform: {
-    '^.+\\.(ts|tsx)$': ['ts-jest', {
-      tsconfig: './tsconfig.test.json'
-    }],
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-    '\\.svg$': 'jest-transform-stub'
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.test.json', // Usa um tsconfig dedicado para testes
+      },
+    ],
+    '^.+\\.[tj]sx?$': 'babel-jest', // Usa o babel-jest para processar arquivos JS e JSX
+    '\\.svg$': 'jest-transform-stub', // Mock para arquivos SVG
   },
   transformIgnorePatterns: [
-    "/node_modules/(?!styled-components)/"
+    'node_modules/(?!(module-to-transform)/)', // Certifique-se de transformar módulos problemáticos aqui
   ],
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], // Configurações globais para testes
   moduleNameMapper: {
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mapeia arquivos CSS para evitar erros
+    '^@/(.*)$': '<rootDir>/src/$1', // Suporte a aliases como @/path
+    '^@/components/(.*)$': '<rootDir>/src/components/$1', // Alias específico para components
+    '\\.svg$': '<rootDir>/__mocks__/fileMock.js', // Mock para SVGs
   },
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
 };
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,6 @@
-import { TextEncoder, TextDecoder } from 'util'
+import { TextEncoder, TextDecoder } from "util"
 import '@testing-library/jest-dom'
 import 'jest-styled-components'
 
-global.TextEncoder = TextEncoder
-global.TextDecoder = TextDecoder
+global.TextEncoder = TextEncoder as typeof globalThis.TextEncoder
+global.TextDecoder = TextDecoder as typeof globalThis.TextDecoder
